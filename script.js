c7a0d73fb8c6bf7fc71851daae271bf3cb158023
Likarios <likarios.lebeks@gmail.com>
2021-12-18 19:55:32 +0100

    Added comments

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// ##########################################################################################################
 // Functions and variables for the image touch grabing
+// ##########################################################################################################
 
 function startDrag(e) {
     this.ontouchmove = this.onmspointermove = moveDrag;
@@ -53,22 +55,29 @@ document.ongesturechange = function() {
     return false;
 }
 
+// ##########################################################################################################
 // Functions and variables for the image tilting based on phone orientation
+// ##########################################################################################################
 
+// Get the event listener on the device orientation
 if ('DeviceOrientationEvent' in window) {
     window.addEventListener('deviceorientation', deviceOrientationHandler, false);
 } else {
     document.getElementsByClassName('container')[0].innerText = 'Device Orientation API not supported.';
 }
 
+// Get the image and apply orientation
 function deviceOrientationHandler(eventData) {
     var tiltLR = eventData.gamma;
     var logo = document.getElementsByClassName("character");
     logo[0].style.transform = "rotate(" + tiltLR + "deg)";
 }
 
+// ##########################################################################################################
 // Functions and variables for the camera capture
+// ##########################################################################################################
 
+// Ask the persmission to access camera and return the stream
 function getUserMedia(options, successCallback, failureCallback) {
     var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
         navigator.mozGetUserMedia || navigator.msGetUserMedia;
@@ -77,6 +86,7 @@ function getUserMedia(options, successCallback, failureCallback) {
     }
 }
 
+// The variable which will hold the captured stream
 var theStream;
 
 function getStream() {
@@ -86,23 +96,22 @@ function getStream() {
         return;
     }
 
+    // The options of the stream
     var constraints = {
         video: true
     };
 
+    // Use the previous function to get the stream
     getUserMedia(constraints, function(stream) {
         var mediaControl = document.querySelector('video');
         if ('srcObject' in mediaControl) {
             mediaControl.srcObject = stream;
-            mediaControl.style.display = "block";
-            containerWidth = window.screen.width - document.querySelector('.container').offsetLeft;
-            containerHeight = window.screen.height - document.querySelector('.container').offsetTop;
         } else {
             mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
-            mediaControl.style.display = "block";
-            containerWidth = window.screen.width - document.querySelector('.container').offsetLeft;
-            containerHeight = window.screen.height - document.querySelector('.container').offsetTop;
         }
+        mediaControl.style.display = "block";
+        containerWidth = window.screen.width - document.querySelector('.container').offsetLeft;
+        containerHeight = window.screen.height - document.querySelector('.container').offsetTop;
         theStream = stream;
     }, function(err) {
         alert('Error: ' + err);
@@ -110,6 +119,7 @@ function getStream() {
 }
 
 function takePhoto() {
+    // Get the video frame and hide it
     var mediaControl = document.querySelector('video');
         if ('srcObject' in mediaControl) {
             mediaControl.style.display = "none";
@@ -117,22 +127,27 @@ function takePhoto() {
             containerHeight = window.screen.height - document.querySelector('.container').offsetTop;
         }
 
+    // Check if the window has allowed the stream
     if (!('ImageCapture' in window)) {
         alert("La capture d'image n'est pas disponible !");
         return;
     }
 
+    // Check if the stream has captured yet
     if (!theStream) {
         alert("Il faut d'abord lancer le stream vidéo !");
         return;
     }
-    var theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
 
-    theImageCapturer.takePhoto()
+    // Initialize the image cpature class with the running stream
+    var imageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
+
+    // Apply the capture to the image and resize it
+    imageCapturer.takePhoto()
         .then(megablob => {
-            var theImageTag = document.getElementsByClassName("character")[0];
-            theImageTag.src = URL.createObjectURL(megablob);
-            theImageTag.style.height = "200px";
+            var imageToReplace = document.getElementsByClassName("character")[0];
+            imageToReplace.src = URL.createObjectURL(megablob);
+            imageToReplace.style.height = "200px";
         })
         .catch(err => alert('Error: ' + err));
 }
